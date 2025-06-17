b7ae019e59c9377b3a172df52afd476e27ddd84c
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-06-17 19:46:38 +0530

    Time: 0 ms (100%), Space: 54.2 MB (72.73%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.ts
@@ -0,0 +1,17 @@
+function bulbSwitch(n: number): number {
+    // let arr=Array(n).fill(0);
+
+    // for( let i=1;i<=n;i++){
+        
+    //    for( let j=i-1;j<n;j+=i){
+    //      arr[j]= arr[j] === 0? 1:0;
+    //    }
+
+    // }
+
+    // return arr.filter((bulb)=> bulb === 1).length;
+    //  time limit excedded 
+        return Math.floor (Math.sqrt (n));
+
+
+};
