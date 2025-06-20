83124fbb8a4f9aa67f221a0328a6eb80d908975e
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-06-20 16:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 58.8 MB (33.17%) - LeetSync

---
+++ 792-binary-search/binary-search.ts
@@ -0,0 +1,8 @@
+function search(nums: number[], target: number): number {
+    for( let i=0;i< nums.length;i++ ){
+        if( nums[i]=== target){
+            return i;
+        }
+    }
+    return -1;
+};
