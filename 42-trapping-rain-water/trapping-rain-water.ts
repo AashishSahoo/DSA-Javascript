4ad3cdca7030010c6806e6cb8e180e5447bed149
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-06-25 00:18:12 +0530

    Time: 1 ms (69.48%) | Memory: 56.9 MB (78.13%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.ts
+++ 42-trapping-rain-water/trapping-rain-water.ts
@@ -1,43 +1,24 @@
-// function trap(height: number[]): number {
-//     // let left = 0, right = height.length-1, result = 0;
-
-//     // while(left<right){
-//     //     let width = right -left;
-//     //     const  ht = Math.min(height[left],height[right]);
-//     //     let area = width *ht;
-//     //     result= Math.max(result,area);
-
-
-//     //     if(height[left] < height[right]){
-//     //         left++;
-//     //     }else{
-//     //         right--;
-//     //     }
-
-//     // }
-//     // return result;
- 
-// };
 
 function trap(height: number[]): number {
-    if (!height || height.length === 0) {
-        return 0;
+   let left =0;
+   let right=height.length -1;
+   let leftMax =0;
+   let rightMax =0;
+
+   let trappedWater =0;
+
+   while( left <right){
+    leftMax = Math.max(leftMax , height[left]);
+    rightMax = Math.max(rightMax , height[right]);
+
+    if(height[left] <height[right]){
+        trappedWater += leftMax - height[left];
+        left++;
+    }else{
+        trappedWater += rightMax - height[right];
+        right--;
     }
-    let l = 0;
-    let r = height.length - 1;
-    let leftMax = height[l];
-    let rightMax = height[r];
-    let res = 0;
-    while (l < r) {
-        if (leftMax < rightMax) {
-            l++;
-            leftMax = Math.max(leftMax, height[l]);
-            res += leftMax - height[l];
-        } else {
-            r--;
-            rightMax = Math.max(rightMax, height[r]);
-            res += rightMax - height[r];
-        }
-    }
-    return res;
+   }
+
+   return trappedWater;
 };
