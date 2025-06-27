89838377290432b0e02c8449eb0639d2f6db377e
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-06-27 11:13:32 +0530

    Time: 3 ms (56.13%) | Memory: 65.9 MB (50.92%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -1,12 +1,14 @@
 function maxProfit(prices: number[]): number {
-    let minPrice = prices[0];
-    let maxProfit = 0;
+  
 
-    for (let i = 1; i < prices.length; i++) {
-        const profit = prices[i] - minPrice;
-        maxProfit = Math.max(maxProfit, profit);
-        minPrice = Math.min(minPrice, prices[i]);
-    }
+ let maxProfit = 0;
+ let minPrice = prices[0];
+  for( let i=0; i< prices.length ;i++){
+    let profit = prices[i]-minPrice;
+    maxProfit = Math.max( maxProfit , profit);
+    minPrice = Math.min(minPrice , prices[i]);
+  }
 
-    return maxProfit;
-}
+  return maxProfit;
+
+};
