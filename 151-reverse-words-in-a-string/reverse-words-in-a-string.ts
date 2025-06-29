ce078331508962f687b55699caa542ff8d70efb0
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-06-30 00:17:12 +0530

    Time: 3 ms (37.19%) | Memory: 57.9 MB (28.28%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -2,8 +2,8 @@ function reverseWords(s: string): string {
   let result = [];
   let word = "";
   for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] !== ' ') {
-      word = s[i] + word;
+    if (s.charAt(i) !== ' ') {
+      word = s.charAt(i) + word;
     } else if (word.length > 0) {
       result.push(word);
       word = "";
