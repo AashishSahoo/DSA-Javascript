d8acb7dfe2cb6302494c73d03b74fcb5fa0c65eb
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-07-01 12:20:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.ts
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.ts
@@ -1,17 +1,9 @@
 function average(salary: number[]): number {
      
-    let min=salary[0];
-    let max=salary[0];
-    let sum=0;
+    let min=Math.min(...salary);
+    let max=Math.max(...salary);
     let len = salary.length ;
-    for( let i=0;i< len ;i++){
-        min = Math.min(min , salary[i]);
-        max = Math.max(max , salary[i]);
-        sum += salary[i]; 
-    }
-
+     let sum = sum.reduce((curr)=> sum+curr  , 0)
     sum = sum - (max+min);
-    
-
      return len >2 ? sum/(len-2):0;
 };
