15dcf6518552f47dd4dacc897fbb89ec3ef41c1d
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-07-02 17:20:44 +0530

    Time: 428 ms (48.77%) | Memory: 86.5 MB (53.97%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.ts
@@ -0,0 +1,26 @@
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    const res: number[] = [];
+    const deque: number[] = []; // stores indices
+
+    for (let i = 0; i < nums.length; i++) {
+        // Remove indices whose corresponding values are less than current value
+        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+
+        // Add current index
+        deque.push(i);
+
+        // Remove indices that are out of the current window
+        if (deque[0] <= i - k) {
+            deque.shift();
+        }
+
+        // If we have a valid window, record the maximum (at front of deque)
+        if (i >= k - 1) {
+            res.push(nums[deque[0]]);
+        }
+    }
+
+    return res;
+}
