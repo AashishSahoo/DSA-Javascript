e736c0aad6d80c3b43a02e2007e97a0ba23c4cea
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-07-04 12:44:49 +0530

    Time: 15 ms (45.92%), Space: 78.7 MB (11.8%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -1,10 +1,16 @@
 function findDuplicate(nums: number[]): number {
-    let mp = new Map<number , number>();
-    for( let i=0;i< nums.length ;i++){
-        mp.set(nums[i] , (mp.get(nums[i])|| 0 ) +1);
-    }
+    // let mp = new Map<number , number>();
+    // for( let i=0;i< nums.length ;i++){
+    //     mp.set(nums[i] , (mp.get(nums[i])|| 0 ) +1);
+    // }
 
-    for( let [key ,value] of mp){
-        if( value >1) return key
+    // for( let [key ,value] of mp){
+    //     if( value >1) return key
+    // }
+
+    let uniquesSet = new Set<number>();
+    for( let i=0;i< nums.length ;i++){
+        if(uniquesSet.has(nums[i]))return nums[i];
+        else uniquesSet.add(nums[i]);
     }
 };
