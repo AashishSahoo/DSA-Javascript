ba381643b39de24b475153eacc50c0c89010834c
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-07-06 15:48:06 +0530

    Time: 0 ms (100%), Space: 55.3 MB (82.75%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.ts
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -1,15 +1,16 @@
 function longestCommonPrefix(strs: string[]): string{
-    if (strs.length === 0) return "";
-    if (strs.length === 1) return strs[0];
+  if( strs.length ===0) return "";
+  if(strs.length ===1 ) return strs[0];
 
-    let prefix = strs[0];
-
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.substring(0, prefix.length - 1);
-            if (prefix === "") return "";
-        }
+  strs =strs.sort();
+  let result:string ="";
+  for(let i=0;i<strs[0].length;i++ ){
+    if( strs[0].charAt(i) === strs[strs.length-1].charAt(i) ){
+        result += strs[0].charAt(i);
+    }else {
+        return result;
     }
+  }
+  return result;
 
-    return prefix;
 };
