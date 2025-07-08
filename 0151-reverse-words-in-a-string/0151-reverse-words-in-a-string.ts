ba9ca48cec12ae647e4d873f8858845455986748
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-07-09 00:50:28 +0530

    Time: 1 ms (80.32%), Space: 57.8 MB (39.85%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -1,14 +1,32 @@
 function reverseWords(s: string): string {
-  let result = [];
-  let word = "";
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] !== ' ') {
-      word = s[i] + word;
-    } else if (word.length > 0) {
-      result.push(word);
-      word = "";
+
+    s= s.trim();
+    let str:string[]= s.split(/\s+/);
+    let i=0;
+    let j= str.length-1;
+
+    while( i<j){
+        let temp= str[i];
+         str[i] = str[j];
+         str[j]=  temp;
+         i++;j--;
     }
-  }
-  if (word.length > 0) result.push(word); 
-  return result.join(" ");
+
+    let result = str.join(" ");
+    return result;
+
+//   let result = [];
+//   let word = "";
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     if (s.charAt(i) !== ' ') {
+//       word = s.charAt(i) + word;
+//     } else if (word.length > 0) {
+//       result.push(word);
+//       word = "";
+//     }
+//   }
+//   if (word.length > 0) result.push(word); 
+//   return result.join(" ");
+
+
 }
