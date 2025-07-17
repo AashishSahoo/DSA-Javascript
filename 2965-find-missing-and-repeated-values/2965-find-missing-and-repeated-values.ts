5bc14671c8f4904637268525235fd47eede2b1e5
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-07-18 00:00:42 +0530

    Time: 5 ms (51.09%), Space: 60.2 MB (40.22%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.ts
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.ts
@@ -1,50 +1,25 @@
-// function findMissingAndRepeatedValues(grid: number[][]): number[] {
-//     let missingNumber:number = -1;
-//     let repatedNumber :number = -1;
-//     // let map:number =new Map<number ,number>();
-//     let mySet = new Set<number>();
-
-//     for( let i=0;i<grid.length ;i++){
-//         for( let j=0;j<grid[i].length;j++){
-//             const val= grid[i][j];
-//             if(mySet.has(val)){
-//                repatedNumber = val; 
-//             }else{
-//                 mySet.add(val);
-//             }
-//         }
-//     }
-
-//     return [missingNumber];
-// };
-
 function findMissingAndRepeatedValues(grid: number[][]): number[] {
-    let repeatedNumber: number = -1;
-    let missingNumber: number = -1;
+    let missingNumber:number = -1;
+    let repatedNumber :number = -1;
+    let mySet = new Set<number>();
 
-    const mySet = new Set<number>();
-    const n = grid.length;
-    const total = n * n;
-
-    // Step 1: Find repeated number
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            const val = grid[i][j];
-            if (mySet.has(val)) {
-                repeatedNumber = val;
-            } else {
+    for( let i=0;i<grid.length ;i++){
+        for( let j=0;j<grid[i].length;j++){
+            const val= grid[i][j];
+            if(mySet.has(val)){
+               repatedNumber = val; 
+            }else{
                 mySet.add(val);
             }
         }
     }
 
-    // Step 2: Find missing number from 1 to n^2
-    for (let i = 1; i <= total; i++) {
-        if (!mySet.has(i)) {
-            missingNumber = i;
-            break;
+    for( let i=1 ;i<=mySet.size * mySet.size ;i++){
+        if( !mySet.has(i)){
+         missingNumber= i; 
+         break;  
         }
     }
 
-    return [repeatedNumber, missingNumber];
-}
+    return [repatedNumber , missingNumber];
+};
