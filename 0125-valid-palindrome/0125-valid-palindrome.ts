1f959b05c52a0b5d581444a0243e1741e381b3fe
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-07-28 18:53:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -0,0 +1,14 @@
+function isPalindrome(s: string): boolean {
+    s= s.toLowerCase();
+    let strArr = s.split("");
+    strArr = strArr.filter((char) => /[a-z]/.test(char) );
+    s = strArr.join("");
+    let i=0;
+    let j= s.length -1;
+    while( i<j){
+        if( s.charAt(i) !== s.charAt(j)) return false
+
+        i++;j--;
+    }
+    return true;
+};
