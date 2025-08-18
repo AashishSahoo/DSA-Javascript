47c76e5ecc95eb5fe864140332e5ed07b685ffd8
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-08-18 10:23:05 +0530

    Time: 8 ms (42.37%) | Memory: 61.8 MB (32.20%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.ts
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.ts
@@ -1,25 +1,29 @@
 function findMissingAndRepeatedValues(grid: number[][]): number[] {
-    let missingNumber:number = -1;
-    let repatedNumber :number = -1;
-    let mySet = new Set<number>();
 
-    for( let i=0;i<grid.length ;i++){
-        for( let j=0;j<grid[i].length;j++){
-            const val= grid[i][j];
-            if(mySet.has(val)){
-               repatedNumber = val; 
-            }else{
-                mySet.add(val);
-            }
-        }
+let missingNumber:number ;
+let repatedNumber : number;
+let total = grid.length * grid.length;
+
+let mp = new Map<number , number>();
+for(let i=0;i< grid.length;i++){
+    for( let j=0;j<grid[i].length ;j++){
+        mp.set(grid[i][j], (mp.get(grid[i][j]) ?? 0) + 1);
+
     }
+}
 
-    for( let i=1 ;i<=mySet.size * mySet.size ;i++){
-        if( !mySet.has(i)){
-         missingNumber= i; 
-         break;  
-        }
-    }
+for( let i=1;i<= total ;i++){
+    let val = mp.get(i) ?? 0;
+     if(val === 0){
+        missingNumber = i;
+     }
+     else if(val === 2){
+        repatedNumber = i;
+     }
 
-    return [repatedNumber , missingNumber];
+}
+
+return [repatedNumber,missingNumber];
+
+
 };
