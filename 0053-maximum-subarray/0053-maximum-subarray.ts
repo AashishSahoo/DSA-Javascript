f1188240db26bce95368a7fbbf0190d57f836fcc
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-15 23:17:32 +0530

    Time: 3 ms (41.57%), Space: 64.9 MB (39.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.ts
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -1,29 +1,15 @@
 function maxSubArray(nums: number[]): number {
-    // if(nums.length ===1) return nums[0];
 
-    // let maxSum = nums[0];
-    // let sum=0;
+   let max= -Infinity;
+   let sum= 0;
+ 
 
-    // for(let i=0;i<nums.length ;i++){
-    //      if(sum <0){
-    //         sum=0;
-    //      }
-    //      sum +=nums[i];
-    //      maxSum = Math.max(maxSum ,sum);
-
-
-    // }
-    // return maxSum;
-
-    let maxSum = nums[0];
-    let sum=0;
-
-    for( let i=0;i<nums.length ;i++){
-        if( sum <0){
-            sum = 0;
-        }
-        sum += nums[i];
-        maxSum= Math.max(sum ,maxSum);
-    }
-    return maxSum;
+   for(let i=0; i<nums.length; i++){
+       if(sum< 0){
+        sum= 0;
+       }
+         sum += nums[i];
+         max = Math.max(sum, max);
+   }
+   return max;
 };
