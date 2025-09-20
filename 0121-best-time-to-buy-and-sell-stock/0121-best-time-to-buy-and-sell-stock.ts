5542088e99f658cbf04ad7e2a0305ff71df27c4c
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-21 02:19:28 +0530

    Time: 1 ms (95.29%), Space: 64.9 MB (95.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,57 +1,30 @@
 function maxProfit(prices: number[]): number {
 
-let res=0 ;
-let max = prices[0];
-let min = prices[0];
 
-for(let i=0; i<prices.length; i++){
-    if( prices[i] < min ){
-        min = prices[i];
-        max = prices[i];
+    let res = 0;
+    let minPrice = prices[0];
 
+    for( let i=1; i< prices.length ;i++){
+         res = Math.max ( (prices[i] - minPrice) , res);
+         minPrice = Math.min(prices[i] , minPrice) ;
     }
-    else if( prices[i] > max){
-        max = prices[i];
-
-    }
-    let sum = max -min;
-    res = Math.max ( sum , res);
-}
-return res;
-
+   return res;
+// let res=0 ;
 // let max = prices[0];
-// let sum =0;
-// for( let i=0;i< prices.length-1 ;i++){
-//      if ( sum < 0){
-//         sum=0;
-//      }
-//      sum = max - prices[i];
-//      max = Math.max(max , sum);
-// }
+// let min = prices[0];
 
-// return max;
-
-
-
-
-
-
-
-
-
-
-
-//     let maxProfit :number=0;
-//     let buyDay =  0;
-//     let sellDay  = 1;
-    
-//     for( let i=0;i< prices.length ;i++){
-//         for( let j=i ;j<prices.length ;j++){
-//              let profit = prices[j] - prices[i];
-//               maxProfit = Math.max( profit , maxProfit);
-//         }
+// for(let i=0; i<prices.length; i++){
+//     if( prices[i] < min ){
+//         min = prices[i];
+//         max = prices[i];
 //     }
+//     else if( prices[i] > max){
+//         max = prices[i];
+//     }
+//     let sum = max -min;
+//     res = Math.max ( sum , res);
+// }
+// return res;
 
-// return maxProfit;
 
 }
