fd2f5d7e61fd14367109e88b4dd016a2370f660d
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-24 13:59:32 +0530

    Time: 4 ms (8.87%), Space: 56.9 MB (7.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.ts
+++ 0075-sort-colors/0075-sort-colors.ts
@@ -2,14 +2,12 @@
  Do not return anything, modify nums in-place instead.
  */
 function sortColors(nums: number[]): void {
-    let n = nums.length;
-      for( let i=0;i< n ;i++){
-        for ( let j=0 ;j< n-i-1 ;j++){
-             if(nums[j] > nums[j+1]){
-                let temp = nums[j+1];
-                nums[j+1]=nums[j];
-                nums[j]= temp;
-             }
+    let n = nums.length ;
+    for( let i=0;i<n ;i++){
+        for( let j=0;j<n-i;j++){
+            if( nums[j] > nums[j+1]){
+                [nums[j] ,nums[j+1]] = [ nums[j+1] ,nums[j]];
+            }
         }
-      }
+    }
 };
