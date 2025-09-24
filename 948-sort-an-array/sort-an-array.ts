b061e8a6c83b50f9a08191670c2296ab12f432e7
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-25 00:26:03 +0530

    Time: 62 ms (73.88%) | Memory: 74.2 MB (88.66%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.ts
@@ -0,0 +1,48 @@
+function sortArray(nums: number[]): number[] {
+        mergeSort( nums ,0 ,nums.length-1);
+        return nums;
+};
+
+function mergeSort(nums ,start , end){
+        if( start >=end){
+        return;
+    }
+
+    let mid = Math.floor((start +end)/2);
+
+    mergeSort( nums , start , mid);
+    mergeSort( nums , mid+1 , end);
+    merge( nums , start , mid ,end);
+ 
+}
+
+function merge( nums , start , mid,end){
+    let temp:number[]=[];
+    let i=start ;
+    let j=mid+1;
+
+    while(i<=mid && j<=end ){
+        if(nums[i] <=nums[j]){
+            temp.push(nums[i]);
+            i++;
+        }else{
+            temp.push(nums[j]);
+            j++;
+        }
+    }
+
+    while( i<=mid){
+        temp.push(nums[i]);
+        i++;
+    }
+    while( j<=end){
+        temp.push(nums[j]);
+        j++;
+    }
+
+   let index = start;
+    for( let k=0;k<temp.length ;k++){
+        nums[index] = temp[k];
+        index++;
+    }
+}
