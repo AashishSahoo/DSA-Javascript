c10538a905a80435084bee71ba2d23fc9d5d536e
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-24 12:50:09 +0530

    Time: 7 ms (22.45%), Space: 60.2 MB (31.85%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -2,21 +2,24 @@
  Do not return anything, modify matrix in-place instead.
  */
 function setZeroes(matrix: number[][]): void {
-    let m:number[]=[]
-    let  n:number[]=[] ;
-    for( let i=0;i< matrix.length ;i++){
-        for ( let j=0;j<matrix[0].length ;j++){
+    let m=[],n=[];
+
+    for( let i=0;i<matrix.length;i++){
+        for( let j=0;j<matrix[i].length ;j++){
             if(matrix[i][j] === 0){
                 m.push(i);
                 n.push(j);
             }
         }
     }
-     for( let i=0;i< matrix.length ;i++){
-        for ( let j=0;j<matrix[0].length ;j++){
-            if( m.includes(i) || n.includes(j)){
-                matrix[i][j]=0;
+
+    for( let i=0;i<matrix.length;i++){
+        for( let j=0;j<matrix[i].length ;j++){
+            if(m.includes(i) || n.includes(j)){
+               matrix[i][j] = 0;
             }
         }
     }
+
+    
 };
