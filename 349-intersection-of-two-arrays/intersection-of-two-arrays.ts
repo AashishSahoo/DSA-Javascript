9175ba3bc94708872ac18bc9273edf2f6b93af4e
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-26 00:52:08 +0530

    Time: 2 ms (48.22%) | Memory: 57.6 MB (33.65%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,32 +1,33 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-    let m1 = new Map<number , number>();
-    let m2 = new Map<number, number>();
+//     let m1 = new Map<number , number>();
+//     let m2 = new Map<number, number>();
 
-    for( let i=0;i< nums1.length ;i++){
-        m1.set( nums1[i] , (m1.get(nums1[i]) ||0) +1);
-    }
-    
-    for( let i=0;i< nums2.length ;i++){
-        m2.set( nums2[i] , (m2.get(nums2[i]) ||0) +1);
-    }
-    let res =[];
-
-    for( let key of m1.keys()){
-        if(m2.has(key)){
-            res.push(key)
-        }
-    }
- return res;
-
-//     let m =nums1.length -1;
-//     let n =nums2.length -1;
-//     let k=nums1;
-//     if( m<n){
-//        k=nums2; 
+//     for( let i=0;i< nums1.length ;i++){
+//         m1.set( nums1[i] , (m1.get(nums1[i]) ||0) +1);
 //     }
+    
+//     for( let i=0;i< nums2.length ;i++){
+//         m2.set( nums2[i] , (m2.get(nums2[i]) ||0) +1);
+//     }
+//     let res =[];
 
-//    let res=k.filter((item) => item === nums2[item]);
-//    return [... new Set(res)];
+//     for( let key of m1.keys()){
+//         if(m2.has(key)){
+//             res.push(key)
+//         }
+//     }
+//  return res;
 
+let s1= new Set(nums1);
+let s2 = new Set(nums2);
+
+let res=[];
+for( let i of s1){
+    if( s2.has(i)){
+        res.push(i);
+    }
+}
+
+return res;
     
 };
