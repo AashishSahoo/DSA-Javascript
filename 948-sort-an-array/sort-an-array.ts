e0a85978b50d17198cf5bb30be62c4fc160ef2a8
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-25 23:42:16 +0530

    Time: 62 ms (73.54%) | Memory: 74.6 MB (86.94%) - LeetSync

--- 948-sort-an-array/sort-an-array.ts
+++ 948-sort-an-array/sort-an-array.ts
@@ -1,48 +1,52 @@
 function sortArray(nums: number[]): number[] {
-        mergeSort( nums ,0 ,nums.length-1);
-        return nums;
+       mergeSort( nums , 0 , nums.length-1);
+       return nums;
 };
 
-function mergeSort(nums ,start , end){
-        if( start >=end){
+
+function mergeSort( nums , start , end){
+    
+    if( start >= end){
         return;
     }
-
-    let mid = Math.floor((start +end)/2);
-
-    mergeSort( nums , start , mid);
-    mergeSort( nums , mid+1 , end);
-    merge( nums , start , mid ,end);
- 
+    let mid = Math.floor( (start+end)/2 );
+    mergeSort( nums, start ,mid);
+    mergeSort( nums,mid+1,end);
+    merge( nums , start ,mid ,end);
 }
 
-function merge( nums , start , mid,end){
-    let temp:number[]=[];
-    let i=start ;
-    let j=mid+1;
 
-    while(i<=mid && j<=end ){
-        if(nums[i] <=nums[j]){
-            temp.push(nums[i]);
-            i++;
-        }else{
-            temp.push(nums[j]);
-            j++;
-        }
-    }
+function merge( nums , start ,mid , end){
+   let i=start;
+   let j= mid+1;
 
-    while( i<=mid){
+   let temp = [];
+
+   while( i<=mid && j<=end){
+    if(nums[i] <= nums[j]){
         temp.push(nums[i]);
         i++;
     }
-    while( j<=end){
+    else{
         temp.push(nums[j]);
         j++;
     }
+   }
 
-   let index = start;
-    for( let k=0;k<temp.length ;k++){
-        nums[index] = temp[k];
-        index++;
-    }
+   while( i<=mid){
+    temp.push(nums[i]);
+    i++;
+   }
+
+   
+   while( j<=end){
+    temp.push(nums[j]);
+    j++;
+   }
+
+   let index=start;
+   for( let k=0;k<temp.length ;k++){
+       nums[index]= temp[k];
+       index++;
+   }
 }
