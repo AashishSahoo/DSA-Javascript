d8f667a82b7a2d25b10f5ae1a1b939c43bb619c8
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-27 01:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (36.04%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.ts
+++ 74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -1,7 +1,7 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
-    for( let i=0;i< matrix.length ;i++){
-        for( let j=0;j< matrix[i].length;j++){
-            if(matrix[i][j] ===  target) return true;
+    for(let i=0;i<matrix.length ;i++){
+        for(let j=0;j<matrix[i].length ;j++){
+            if(matrix[i][j]===target) return true
         }
     }
     return false;
