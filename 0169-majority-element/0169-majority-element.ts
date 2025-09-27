91422127e49740e3fa203340bed4c76215c1947a
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-27 14:28:39 +0530

    Time: 6 ms (36.43%), Space: 57.5 MB (85.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,14 +1,15 @@
 function majorityElement(nums: number[]): number {
-    let mp=new Map<number,number>();
 
-    for( let i=0;i<nums.length ;i++){
-        mp.set(nums[i], (mp.get(nums[i]) || 0) + 1);
+   let mp= new Map<number,number>();
 
-
+   for( let i=0;i<nums.length;i++){
+    mp.set(nums[i] , (mp.get(nums[i])||0) +1);
+   }
+   for( let [key , val] of mp){
+    if(val>(nums.length/2)){
+        return key;
     }
-    for( let [key ,value] of mp){
-        if(value > (nums.length/2)){
-            return key;
-        }
-    }
+   }
+   
+
 };
