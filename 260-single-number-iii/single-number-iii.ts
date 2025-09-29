6b848b11f6f3b57a40cbed6a9fd3ee9cbe98e4c7
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-29 21:53:38 +0530

    Time: 6 ms (49.23%) | Memory: 60 MB (13.85%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.ts
@@ -0,0 +1,14 @@
+function singleNumber(nums: number[]): number[] {
+   let myMap = new Map<number , number>();
+   let res:number[]=[]
+  for(let i=0;i< nums.length;i++){
+      myMap.set(nums[i] , (myMap.get(nums[i]) || 0)+1 );
+  }
+
+  for( let [key,value] of myMap){
+    if( value === 1){
+        res.push(key);
+    }
+  }
+  return res;
+};
