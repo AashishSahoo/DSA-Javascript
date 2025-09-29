52858be46082b257df37739a044f7aefab1929c1
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-09-29 21:51:20 +0530

    Time: 4 ms (67.16%) | Memory: 59.7 MB (12.69%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.ts
@@ -0,0 +1,13 @@
+function singleNumber(nums: number[]): number {
+    let mp= new Map();
+    for( let i=0;i<nums.length;i++){
+        mp.set( nums[i] , (mp.get(nums[i])||0)+1);
+    }
+
+    for( let [key,val] of mp){
+        if( val===1){
+            return key;
+        }
+    }
+    return 0;
+};
