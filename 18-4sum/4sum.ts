ad2e06a7df6115c2a247bfe91b285cc93b2239f0
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-01 19:22:38 +0530

    Time: 18 ms (58.54%) | Memory: 59.3 MB (65.33%) - LeetSync

--- 18-4sum/4sum.ts
+++ 18-4sum/4sum.ts
@@ -1,33 +1,37 @@
 function fourSum(nums: number[], target: number): number[][] {
-  if( nums.length < 4)return [];
-   nums = nums.sort((a,b)=> a-b);
-  let res : number [][]=[];
+  let res: number[][] = [];
+  nums.sort((a, b) => a - b);
 
-  for( let i=0;i<nums.length -3 ;i++){
-    if( i>0 && nums[i] === nums[i-1]) continue;
-    for(  let j=i+1 ;j< nums.length -2;j++ ){
-        if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+  if (nums.length < 4) return [];
 
-        let p = j+1;
-        let q= nums.length -1;
+  for (let i = 0; i < nums.length - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue; // skip duplicates for i
 
-        while( p<q){
-            
-            let sum = nums[i] + nums[j] + nums[p] + nums[q] ;
-            if( sum < target){
-                p++;
-            }else if( sum > target ){
-                q--;
-            }else if( sum === target){
-                res.push([nums[i] , nums[j] , nums[p] , nums[q]]);
-                while (p<q && nums[p] === nums[p+1]){p++};
-                while (p<q && nums[q] === nums[q-1]){q--};
-                p++;
-                q--;
-            }
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue; // skip duplicates for j
+
+      let p = j + 1;
+      let q = nums.length - 1;
+
+      while (p < q) {
+        let sum = nums[i] + nums[j] + nums[p] + nums[q];
+
+        if (sum === target) {
+          res.push([nums[i], nums[j], nums[p], nums[q]]);
+          p++;
+          q--;
+
+          while (p < q && nums[p] === nums[p - 1]) p++; // skip dupes
+          while (p < q && nums[q] === nums[q + 1]) q--; // skip dupes
+
+        } else if (sum < target) {
+          p++;
+        } else {
+          q--;
         }
-        
+      }
     }
-  } 
+  }
+
   return res;
 }
