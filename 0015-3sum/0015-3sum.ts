4a10e161ab9982314b7714d3cfbb0c7c5f71ae97
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-01 20:13:42 +0530

    Time: 34 ms (76.29%), Space: 72.6 MB (80.41%) - LeetHub

--- 0015-3sum/0015-3sum.ts
+++ 0015-3sum/0015-3sum.ts
@@ -1,84 +1,42 @@
-// function threeSum(nums: number[]): number[][] {
-//     let res: number[][] = [];
-
-//     for (let i = 0; i < nums.length - 2; i++) {
-//         for (let j = i + 1; j < nums.length - 1; j++) {
-//             for (let k = j + 1; k < nums.length; k++) {
-//                 if (nums[i] + nums[j] + nums[k] === 0) {
-//                     let triplet = [nums[i], nums[j], nums[k]].sort((a, b) => a - b);
-
-//                     let isDuplicate = false;
-//                     for (let existing of res) {
-//                         if (
-//                             existing[0] === triplet[0] &&
-//                             existing[1] === triplet[1] &&
-//                             existing[2] === triplet[2]
-//                         ) {
-//                             isDuplicate = true;
-//                             break;
-//                         }
-//                     }
-
-//                     if (!isDuplicate) {
-//                         res.push(triplet);
-//                     }
-//                 }
-//             }
-//         }
-//     }
-
-//     return res;
-// }
-
-function merge(leftArr: number[], rightArr: number[]) {
-    const output = [];
-    let i = 0;
-    let j = 0;
-    while (i < leftArr.length && j < rightArr.length) {
-        if (leftArr[i] < rightArr[j]) {
-            output.push(leftArr[i++])
-        } else {
-            output.push(rightArr[j++])
-        }
-    }
-
-    return [...output, ...leftArr.slice(i), ...rightArr.slice(j)];
-}
-
-function mergeSort(nums: number[]): number[] {
-    if (nums.length < 2) {
-        return nums
-    }
-    const mid = Math.floor(nums.length / 2);
-    return merge(mergeSort(nums.slice(0, mid)), mergeSort(nums.slice(mid)))
-};
-
 function threeSum(nums: number[]): number[][] {
-    const sortedNums = mergeSort(nums);
-    const output = [];
+   
+   let res:number[][]=[];
+   if(nums.length <3)return [];
+   
+   nums=nums.sort((a,b)=> a-b);
 
-    for (let i = 0; i < sortedNums.length && sortedNums[i] <= 0; i++) {
-        if (i > 0 && sortedNums[i] === sortedNums[i - 1]) {
-            continue;
-        }
-        const target = -(sortedNums[i]);
-        let left = i + 1;
-        let right = sortedNums.length - 1;
-        while (left < right) {
-            const sum = sortedNums[left] + sortedNums[right];
-            if (target === sum) {
-                output.push([sortedNums[i], sortedNums[left], sortedNums[right]]);
-                left++
-                right--
-                while (left < right && sortedNums[left] === sortedNums[left - 1]) {
-                    left++
-                }
-            } else if (target > sum) {
-                left++
-            } else {
-                right--
+   for(let i=0;i<nums.length-2;i++){
+     if( i>0 && nums[i]===nums[i-1]) continue;
+
+         let p=i+1;
+         let q=nums.length-1;
+
+         while(p<q){
+            let sum = nums[i]+nums[p]+nums[q];
+                    
+            if(sum === 0){
+                let temp:number[] =[nums[i],nums[p],nums[q]];
+                res.push(temp);
+                p++;
+                q--;
+
+                 while( p<q && nums[p]===nums[p-1]){
+                p++;
             }
-        }
-    }
-    return output;
+
+            while( p<q && nums[q]===nums[q+1]){
+                q--;
+            }
+               
+            }
+            else if(sum < 0){
+                 p++;
+            }
+            else{
+                q--;
+            }
+         
+         }
+   }
+   return res;
 };
