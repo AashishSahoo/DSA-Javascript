47635b74ef2d095f2a5cebad674322298432c561
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-01 19:23:15 +0530

    Time: 18 ms (58.54%), Space: 59.3 MB (65.33%) - LeetHub

--- 0018-4sum/0018-4sum.ts
+++ 0018-4sum/0018-4sum.ts
@@ -1,46 +1,37 @@
 function fourSum(nums: number[], target: number): number[][] {
-  let res:number[][]=[];
+  let res: number[][] = [];
+  nums.sort((a, b) => a - b);
 
-  nums=nums.sort((a,b)=> a-b);
+  if (nums.length < 4) return [];
 
-  if(nums.length<4){
-    return [];
+  for (let i = 0; i < nums.length - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue; // skip duplicates for i
+
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue; // skip duplicates for j
+
+      let p = j + 1;
+      let q = nums.length - 1;
+
+      while (p < q) {
+        let sum = nums[i] + nums[j] + nums[p] + nums[q];
+
+        if (sum === target) {
+          res.push([nums[i], nums[j], nums[p], nums[q]]);
+          p++;
+          q--;
+
+          while (p < q && nums[p] === nums[p - 1]) p++; // skip dupes
+          while (p < q && nums[q] === nums[q + 1]) q--; // skip dupes
+
+        } else if (sum < target) {
+          p++;
+        } else {
+          q--;
+        }
+      }
+    }
   }
 
-   for( let i=0;i<nums.length ;i++){
-      if( nums[i]===nums[i-1] && i>0 ) continue;
-
-    for( let j=i+1 ;j<nums.length;j++){
-      if (j > i + 1 && nums[j] === nums[j - 1]) continue; 
-
-        let p=j+1;
-        let q=nums.length-1;
-
-        while(p<q){
-
-            let sum = nums[i]+nums[j]+nums[p]+nums[q];
-            let temp = [nums[i],nums[j],nums[p],nums[q]];
-        
-            if(sum === target  ){
-                res.push(temp);
-                p++;
-                q--;
-                while(nums[p]===nums[p-1] && p<q){
-                    p++;
-                }
-                 while(nums[q]===nums[q-1] && p<q){
-                    q--;
-                }
-            }else if(  sum < target){
-               p++;
-            }else{
-                q--;
-            }
-        }
-        
-        
-    }
-   }
-   
-   return res;
+  return res;
 }
