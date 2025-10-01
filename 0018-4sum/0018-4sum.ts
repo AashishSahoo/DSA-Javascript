2751cff90f4f73571e582437eac67df5060cf38b
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-01 19:22:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.ts
+++ 0018-4sum/0018-4sum.ts
@@ -1,33 +1,46 @@
 function fourSum(nums: number[], target: number): number[][] {
-  if( nums.length < 4)return [];
-   nums = nums.sort((a,b)=> a-b);
-  let res : number [][]=[];
+  let res:number[][]=[];
 
-  for( let i=0;i<nums.length -3 ;i++){
-    if( i>0 && nums[i] === nums[i-1]) continue;
-    for(  let j=i+1 ;j< nums.length -2;j++ ){
-        if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+  nums=nums.sort((a,b)=> a-b);
 
-        let p = j+1;
-        let q= nums.length -1;
+  if(nums.length<4){
+    return [];
+  }
 
-        while( p<q){
-            
-            let sum = nums[i] + nums[j] + nums[p] + nums[q] ;
-            if( sum < target){
+   for( let i=0;i<nums.length ;i++){
+      if( nums[i]===nums[i-1] && i>0 ) continue;
+
+    for( let j=i+1 ;j<nums.length;j++){
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue; 
+
+        let p=j+1;
+        let q=nums.length-1;
+
+        while(p<q){
+
+            let sum = nums[i]+nums[j]+nums[p]+nums[q];
+            let temp = [nums[i],nums[j],nums[p],nums[q]];
+        
+            if(sum === target  ){
+                res.push(temp);
                 p++;
-            }else if( sum > target ){
                 q--;
-            }else if( sum === target){
-                res.push([nums[i] , nums[j] , nums[p] , nums[q]]);
-                while (p<q && nums[p] === nums[p+1]){p++};
-                while (p<q && nums[q] === nums[q-1]){q--};
-                p++;
+                while(nums[p]===nums[p-1] && p<q){
+                    p++;
+                }
+                 while(nums[q]===nums[q-1] && p<q){
+                    q--;
+                }
+            }else if(  sum < target){
+               p++;
+            }else{
                 q--;
             }
         }
         
+        
     }
-  } 
-  return res;
+   }
+   
+   return res;
 }
