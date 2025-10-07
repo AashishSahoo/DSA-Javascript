bf8ba6ff44fda9517668683cf48f290768d1d841
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-07 11:08:42 +0530

    Time: 1546 ms (14.08%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,10 +1,4 @@
-
-
-
-
-
-
-select p.product_name  , s.year , s.price
+select p.product_name , s.year , s.price
 from Sales as s
-join Product as p
+left join Product as p
 on s.product_id  = p.product_id  
