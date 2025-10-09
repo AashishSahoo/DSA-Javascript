387fc3595cb27ff2ac43dfe7249fdb06c4dc47e7
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-09 10:42:39 +0530

    Time: 0 ms (100.00%) | Memory: 55.9 MB (7.91%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.ts
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.ts
@@ -11,15 +11,14 @@
  */
 
 function middleNode(head: ListNode | null): ListNode | null {
-    let slow:ListNode | null= head;
-    let fast:ListNode | null= head;
 
-    while( fast!==null && fast.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-    }
-    return slow;
+let slow: ListNode | null = head;
+let fast : ListNode | null=head;
 
-
+while( fast!==null &&  fast.next!==null ){
+    slow=slow.next;
+    fast=fast.next.next;
+}
+return slow;
 
 };
