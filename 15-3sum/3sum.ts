36dc166b8eb2fd968e9ca23b8d79071768e01e6a
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-11 10:50:55 +0530

    Time: 34 ms (75.39%) | Memory: 74.4 MB (22.82%) - LeetSync

--- 15-3sum/3sum.ts
+++ 15-3sum/3sum.ts
@@ -1,42 +1,34 @@
 function threeSum(nums: number[]): number[][] {
    
-   let res:number[][]=[];
-   if(nums.length <3)return [];
-   
-   nums=nums.sort((a,b)=> a-b);
+    let res:number[][]=[];
 
-   for(let i=0;i<nums.length-2;i++){
-     if( i>0 && nums[i]===nums[i-1]) continue;
+    nums= nums.sort((a,b)=> a-b);
 
-         let p=i+1;
-         let q=nums.length-1;
+    for(let i=0;i<nums.length-2;i++){
 
-         while(p<q){
-            let sum = nums[i]+nums[p]+nums[q];
-                    
-            if(sum === 0){
-                let temp:number[] =[nums[i],nums[p],nums[q]];
-                res.push(temp);
-                p++;
-                q--;
+        if(i>0 && nums[i]===nums[i-1]){continue}
+        let start = i+1;
+        let end = nums.length-1;
 
-                 while( p<q && nums[p]===nums[p-1]){
-                p++;
-            }
+        while(start<end){
+            if( nums[i] + nums[start] + nums[end] === 0){
+                res.push([nums[i] , nums[start] , nums[end]]);
+                start++;
+                end--;
 
-            while( p<q && nums[q]===nums[q+1]){
-                q--;
+
+                while(start<end && nums[start]===nums[start-1] ){
+                    start++;
+                }
+                while(start<end && nums[end]===nums[end+1] ){
+                    end--;
+                }
+            }else if( nums[i] + nums[start] + nums[end] <0){
+                start++;
+            }else{
+                end--;
             }
-               
-            }
-            else if(sum < 0){
-                 p++;
-            }
-            else{
-                q--;
-            }
-         
-         }
-   }
-   return res;
+        }
+    }
+    return res;
 };
