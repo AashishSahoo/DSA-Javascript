ae2c739db696894f5538681ee20a38d6a3a573ba
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-17 12:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 57.4 MB (95.29%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,11 +1,12 @@
 function removeDuplicates(nums: number[]): number {
-    if (nums.length === 0) return 0;
 
+    if(nums.length === 0) return 0;
     let i=0;
-    for( let j=1;j< nums.length ;j++){
-        if(nums[j] !== nums[i]){
+
+    for(let k=1;k<nums.length;k++){
+        if(nums[k]!==nums[i]){
             i++;
-            nums[i]=nums[j];
+            nums[i]=nums[k];
         }
     }
     return i+1;
