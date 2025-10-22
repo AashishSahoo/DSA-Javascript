6ae2d21c7effaed05edb5166982c489a29cd3355
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-22 21:59:25 +0530

    Time: 1 ms (74.07%) | Memory: 55.8 MB (62.96%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.ts
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.ts
@@ -1,12 +1,20 @@
 function removeOccurrences(s: string, part: string): string {
 
-    while(s.includes(part)){
-       
-       s=s.replace(part,"");
-    
-    }
 
-    return s;
+    let finalStr = s;
+
+  while (finalStr.includes(part)) {
+    let idx = finalStr.indexOf(part); // find where the part starts
+    finalStr =
+      finalStr.slice(0, idx) + finalStr.slice(idx + part.length); // remove it
+  }
+
+  return finalStr;
+    // while(s.includes(part)){
+    //    s=s.replace(part,"");
+    // }
+
+    // return s;
 
     
 };
