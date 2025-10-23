133e34d1988ca8df2e5d748e00fc56617087c2f6
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-24 00:55:37 +0530

    Time: 0 ms (100%), Space: 55.4 MB (84.03%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -1,29 +1,25 @@
 
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
    
+    let k=m+n-1;
+    let i=m-1;
+    let j=n-1;
 
- let k=m+n-1;
- let i=m-1;
- let j=n-1;
-
- while(i>=0 && j>=0){
-    if(nums1[i] >= nums2[j]){
-        nums1[k]=nums1[i];
-        i--;
-    }else{
-        nums1[k]=nums2[j];
-        j--;
+    while(i>=0 && j>=0){
+         if( nums1[i] > nums2[j]){
+            nums1[k] = nums1[i];
+            i--;
+         }
+         else {
+            nums1[k] = nums2[j];
+            j--;
+         }
+         k--;
     }
-    k--;
- }
-
-
- while(i>=0){
-    nums1[k--]=nums1[i--];
- }
- while(j>=0){
-    nums1[k--]=nums2[j--];
- }
- 
+    while( j>=0){
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
 
 };
