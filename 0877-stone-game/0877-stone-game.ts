670e5db3f51c54baffd56a21666dd762ebede7b2
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-10-25 16:16:46 +0530

    Time: 2 ms (23.53%), Space: 54.8 MB (88.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.ts
+++ 0877-stone-game/0877-stone-game.ts
@@ -1,28 +1,27 @@
 function stoneGame(piles: number[]): boolean {
-//     let alice:number[] =[];
-//     let bob:number[] =[];
-//     let i=0;
-//     piles = piles.sort((a,b)=> a-b);
+    let alice:number[] =[];
+    let bob:number[] =[];
+    let i=0;
+    piles = piles.sort((a,b)=> a-b);
 
-//    while(piles.length>0){
-//       if(i%2!==0){
-//         let max =piles[0];
-//         piles.shift();
-//         alice.push(max);
-//         i++;
+   while(piles.length>0){
+      if(i%2!==0){
+        let max =piles[0];
+        piles.shift();
+        alice.push(max);
+        i++;
 
-//       }else{
-//         let max =piles[0];
-//         piles.shift();
-//         bob.push(max);
-//         i++;
+      }else{
+        let max =piles[0];
+        piles.shift();
+        bob.push(max);
+        i++;
         
-//       }
-//    }
-//    let aliceTot = alice.reduce( (sum,item) =>sum+=item  , 0);
-//    let bobTot = bob.reduce( (sum,item) =>sum+=item  , 0);
+      }
+   }
+   let aliceTot = alice.reduce( (sum,item) =>sum+=item  , 0);
+   let bobTot = bob.reduce( (sum,item) =>sum+=item  , 0);
 
-//     return (aliceTot > bobTot) ? true : false;
- 
- return true;
+    return (aliceTot > bobTot) ? true : false;
+
 };
