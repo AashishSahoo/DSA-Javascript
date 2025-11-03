8f7a74d15cfb107b39dba3df522790b45a1ee5f3
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-11-04 00:25:43 +0530

    Time: 6 ms (95.45%), Space: 64 MB (95.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.ts
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -1,18 +1,19 @@
 function merge(intervals: number[][]): number[][] {
+  if (intervals.length === 0) return [];
 
-   intervals.sort((a,b) => a[0] -b[0]);
-   let merged = [];
-   let prev = intervals[0];
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res: number[][] = [];
+  let prev = intervals[0];
 
-   for(let i=1;i<intervals.length ;i++){
-      if(intervals[i][0] <= prev[1]){
-            prev[1] = Math.max( prev[1] , intervals[i][1]);
-       }else{
-            merged.push(prev);
-            prev = intervals[i];
-        }
+  for (let i = 1; i < intervals.length; i++) {
+    if (prev[1] >= intervals[i][0]) {
+      prev[1] = Math.max(prev[1], intervals[i][1]); 
+    } else {
+      res.push(prev);
+      prev = intervals[i];
     }
-   merged.push(prev);
-   return merged;
+  }
 
+  res.push(prev);
+  return res;
 }
