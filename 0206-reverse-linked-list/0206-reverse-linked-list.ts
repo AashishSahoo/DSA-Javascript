732675c15bd061ed05ec3632692ef6be5bc90d06
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-11-04 13:45:45 +0530

    Time: 0 ms (100%), Space: 58.5 MB (57.43%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.ts
+++ 0206-reverse-linked-list/0206-reverse-linked-list.ts
@@ -1,13 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
 function reverseList(head: ListNode | null): ListNode | null {
-   let prev: ListNode | null = null;
-   let curr: ListNode | null = head;
 
-   while (curr !== null) {
-      let nxt: ListNode | null = curr.next; 
-      curr.next = prev;                     
-      prev = curr;                          
-      curr = nxt;                           
-   }
+    let curr = head;
+    let prev= null;
 
-   return prev; 
-}
+    while(curr){
+        let next =curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr= next;
+
+    }
+    return prev;
+    
+};
