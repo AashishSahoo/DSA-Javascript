8b5dad247aaa42cc980655790c43e258b621c443
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-11-11 15:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 65.4 MB (36.47%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.ts
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.ts
@@ -1,19 +1,9 @@
 function peakIndexInMountainArray(arr: number[]): number {
-    // let mp = new Map();
 
-    // for( let i=0;i<arr.length ;i++){
-    //     mp.set( arr[i] , (mp.get(arr[i])||0)+1 );
-    // }
-
-
-    let idx=null;
-    let max=arr[0];
-
-    for( let i=1;i<arr.length;i++){
-        if(arr[i]>max){
-            max = arr[i];
-            idx=i;
+    for(let i=1;i<arr.length-1;i++){
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+            return i;
         }
     }
-    return idx;
+    return  null;
 };
