142852d48941a283bc1286825edf4262e90ae3be
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-11-24 18:11:44 +0530

    Time: 2 ms (67.73%) | Memory: 61.1 MB (41.96%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.ts
+++ 485-max-consecutive-ones/max-consecutive-ones.ts
@@ -2,15 +2,17 @@ function findMaxConsecutiveOnes(nums: number[]): number {
    let max=0;
    let currMax=0;
 
-   for( let i=0;i<nums.length;i++){
-    if(nums[i]===1){
-        currMax++;
-        max=Math.max(max,currMax);
 
-    }else{
-        max=Math.max(max,currMax);
-        currMax=0;
-    }
-   }  
+   for( let i=0;i<nums.length;i++){
+       if(nums[i] === 1){
+            currMax++;
+            max= Math.max(currMax,max);
+
+        }else{
+            currMax=0;            
+       }
+   }
+
+
    return max;
 };
