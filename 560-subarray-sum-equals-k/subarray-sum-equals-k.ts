6b937800fa2f0b3f906ece1a99733a81a8f98452
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-11-27 14:50:12 +0530

    Time: 1599 ms (6.25%) | Memory: 58.1 MB (94.79%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.ts
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.ts
@@ -1,15 +1,15 @@
 function subarraySum(nums: number[], k: number): number {
-    let count = 0;
+    let cnt=0;
 
-    for (let i = 0; i < nums.length; i++) {
-        let sum = 0;
-        for (let j = i; j < nums.length; j++) {
-            sum += nums[j];
-            if (sum === k) {
-                count++;
-            }
+    for(let i=0;i<nums.length;i++){
+        let sum=0;
+
+        for(let j=i;j<nums.length;j++ ){
+            sum+=nums[j]
+             if(sum ===k){
+                cnt++;
+             }
         }
     }
-
-    return count;
-}
+    return cnt;
+};
