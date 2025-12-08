9a63b809676b15fd3f7fdf5d0600ac8a06c9d624
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-08 14:43:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.ts
@@ -0,0 +1,20 @@
+function search(nums: number[], target: number): number {
+    
+    let i=0;
+    let j=nums.length-1;
+
+    while(i<j){
+        let mid= Math.floor((i+j)/2);
+
+        if(nums[mid]=== target){
+                        return mid;
+        }
+
+        if(target >nums[mid]){
+            i=mid+1;
+        }else if( target<nums[mid]){
+            j=mid-1;
+        }
+    }
+    return -1;
+};
