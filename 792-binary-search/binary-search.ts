400db7340b9eb6d0fe3b35631ea683291bb067d0
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-08 14:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 58.8 MB (36.53%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,7 +1,19 @@
 function search(nums: number[], target: number): number {
-    for( let i=0;i< nums.length;i++ ){
-        if( nums[i]=== target){
-            return i;
+    
+    let i=0;
+    let j=nums.length-1;
+
+    while(i<=j){
+        let mid= Math.floor((i+j)/2);
+
+        if(nums[mid]=== target){
+                        return mid;
+        }
+
+        if(target >nums[mid]){
+            i=mid+1;
+        }else if( target<nums[mid]){
+            j=mid-1;
         }
     }
     return -1;
