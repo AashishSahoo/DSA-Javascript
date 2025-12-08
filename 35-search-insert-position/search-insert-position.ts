edd54e936e6e9c29157909138b9b845919047ea8
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-08 15:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (19.00%) - LeetSync

--- 35-search-insert-position/search-insert-position.ts
+++ 35-search-insert-position/search-insert-position.ts
@@ -1,18 +1,16 @@
 function searchInsert(nums: number[], target: number): number {
-    let left = 0;
-    let right = nums.length - 1;
+    let i=0;
+    let j=nums.length-1;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
+    while(i<=j){
+        let mid = Math.floor((i+j)/2);
+        if(nums[mid]=== target){
             return mid;
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        }else if( nums[mid]> target){
+            j=mid-1;
+        }else{
+            i=mid+1;
         }
     }
-
-    return left;
-}
+    return i;
+};
