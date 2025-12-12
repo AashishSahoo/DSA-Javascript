b8f291915055df1f05c17936b2f837e0ec946dac
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-12 18:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (48.28%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.ts
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.ts
@@ -1,20 +1,16 @@
 function maxAscendingSum(nums: number[]): number {
+    let maxSum = nums[0];
+    let currentSum = nums[0];
 
-  let maxSum = nums[0];
-  let currSum = maxSum;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            currentSum += nums[i];  
+        } else {
+            currentSum = nums[i];    
+        }
 
-  for( let i=1;i< nums.length ;i++){
-    if( nums[i] > nums[i-1]){
-        currSum += nums[i]
-    }else{
-        currSum = nums[i]
+        maxSum = Math.max(maxSum, currentSum);
     }
 
-    if( maxSum < currSum){
-        maxSum = currSum;
-    }
-  }
-
-return maxSum;
-
-};
+    return maxSum;
+}
