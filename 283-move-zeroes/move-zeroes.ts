ecf5d0deed717d119781e052f1578e1d8e395c3b
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-12 19:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 61.1 MB (54.98%) - LeetSync

--- 283-move-zeroes/move-zeroes.ts
+++ 283-move-zeroes/move-zeroes.ts
@@ -2,38 +2,17 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-   let index=0;
-
-   for( let i=0;i<nums.length ;i++){
-    if(nums[i] !== 0){
-        nums[index] = nums[i];
-        index++;
+    let j=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!== 0){
+            nums[j]=nums[i];
+            j++;
+        }
     }
-   }
-   
-   while(index < nums.length){
-    nums[index] =0;
-    index++;
-   }
 
-    // let zeroCount =0;
-    // for( let i=0;i< nums.length ;i++){
-    //       if(nums[i] === 0){
-            
-    //         zeroCount++;
-    //       }
-    // }
-    // nums = nums.sort((a,b) => a-b);
-
-    // for( let i=0;i<zeroCount;i++){
-    //     nums.shift();
-    // }
-
-    // for( let i=0;i<zeroCount;i++){
-    //     nums.push(0);
-    // }
-        
-
-
-
+    while(j<nums.length){
+        nums[j]=0;
+        j++;
+    }
+    
 };
