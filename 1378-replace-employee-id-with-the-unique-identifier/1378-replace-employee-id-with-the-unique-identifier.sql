11ab2159778efdcafdb941c62314a9800c50e7ff
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-13 20:09:03 +0530

    Time: 993 ms (95.16%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,7 @@
-select  eu.unique_id  as unique_id  , e.name
+# Write your MySQL query statement below
+
+select  u.unique_id     as unique_id ,e.name as name
 from Employees as e
-left join EmployeeUNI as eu
-on e.id=eu.id
--- group by  e.name 
+left join EmployeeUNI as u
+on e.id =u.id
+group by e.id
