0fbeb6c0cc7ca788e0ccd704cf425f2cf7ad22a6
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-13 19:45:42 +0530

    Time: 13 ms (9.48%) | Memory: 65 MB (5.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -1,13 +1,12 @@
 function isPalindrome(s: string): boolean {
-    s= s.toLowerCase();
-    let strArr = s.split("");
-    strArr = strArr.filter((char) => /[a-z0-9]/.test(char) );
-    s = strArr.join("");
-    let i=0;
-    let j= s.length -1;
-    while( i<j){
-        if( s.charAt(i) !== s.charAt(j)) return false
-
+    s=s.toLowerCase();
+    let strArr= s.split("");
+    let str= strArr.filter((char)=> /[a-z0-9]/.test(char));
+    // str=str.join("");
+   let i=0;
+   let j=str.length-1;
+    while(i<j){
+        if(str[i]!== str[j])return false;
         i++;j--;
     }
     return true;
