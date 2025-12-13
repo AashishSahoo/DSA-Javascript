5a1b82d86a0738e6eb45d142438327b638ce019e
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-13 19:31:33 +0530

    Time: 45 ms (63.11%) | Memory: 54.7 MB (72.59%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.ts
+++ 1013-fibonacci-number/fibonacci-number.ts
@@ -1,43 +1,16 @@
 function fib(n: number): number {
 
-    if(n === 0) return 0;
-    if(n === 1) return 1;
 
-    let a = 0;
-    let b = 1;
-    let sum =a+b;
+   if(n===0) return 0;
+   if(n===1) return 1;
 
-    for( let i=0;i< n-2;i++){
-    
-       let c= a+b;
-       sum = c+b;
-       a=b;
-       b=c;
+   let a=0;
+   let b=1;
 
-    }
-   return sum
+   for( let i=2;i<=n;i++){
+    let c=a+b;
+    a=b;
+    b=c;
+   }
+   return b;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
