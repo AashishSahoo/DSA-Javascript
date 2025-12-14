2d726744024aee78dea0488de1c469e66d44cc9c
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-14 20:15:29 +0530

    Time: 16 ms (44.37%), Space: 61.3 MB (19.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,16 +1,20 @@
 function isAnagram(s: string, t: string): boolean {
-    if(s.length !== t.length ) return false;
-   let map = new Map<string , number>();
+    
+    if(s.length !== t.length) return false;
 
-   for( let char of s){
-      map.set( char , (map.get(char)?? 0)+1);
-   }
+    let sm=new Map();
+    for(let char of s){
+        sm.set( char , (sm.get(char) ||0)+1 );
+    }
 
-   for( let char of t){
-    if(!map.has(char)) return false
-    map.set( char , map.get(char) -1 );
-    if(map.get(char) <0) return false
-   }
-   return true;
+
+    for(let char of t){
+        if(!sm.has(char))return false;
+
+        sm.set(char ,sm.get(char)-1);
+        if(sm.get(char)<0) return false;
+
+    }
+    return true;
 
 };
