3335876cc7462f0ae210694ebb4b1dd7bdb2a6e9
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2025-12-14 20:08:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.ts
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -1,25 +1,17 @@
-function longestCommonPrefix(strs: string[]): string{
-  if(strs.length === 0 ){
-    return ""
-  }
-  if(strs.length ===1){
-    return strs[0];
-  }
+function longestCommonPrefix(strs: string[]): string {
 
-   strs = strs.sort();
-   let prefix = "";
-   let check = true;
-   let i=0;
-   let j=0;
-   while(true){
-    if(strs[0].charAt(i) === strs[strs.length -1].charAt(j)){
-        prefix +=strs[0].charAt(i) ;
-        i++;
-        j++;
-    }else if(strs[0].charAt(i) !== strs[strs.length -1].charAt(j)){
-        break;
+     if( strs.length ===0) return "";
+  if(strs.length ===1 ) return strs[0];
+ 
+    strs= strs.sort();
+    let a=strs[0];
+    let b=strs[strs.length-1];
+
+    let res="";
+    for(let i=0;i<a.length;i++){
+       if(a[i]===b[i]) res+=a[i];
+       else break;
+
     }
-   }
-   return prefix;
-
+    return res.length>1? res:"";
 };
