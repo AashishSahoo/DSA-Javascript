e2a43602eb2a29da3401f7d2d7ae5108e3ced885
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-01-02 20:23:05 +0530

    Time: 1 ms (88.34%), Space: 58.6 MB (57.01%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.ts
@@ -0,0 +1,13 @@
+function pivotIndex(nums: number[]): number {
+   let totalSum = nums.reduce((item,sum)=> sum+=item ,0);
+   let leftSum=0;
+
+
+   for(let i=0;i<nums.length;i++){
+    let rightSum = totalSum - leftSum - nums[i];
+    if(leftSum === rightSum )return i;
+
+    leftSum +=nums[i];
+   }
+   return -1;
+};
