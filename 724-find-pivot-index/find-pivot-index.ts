b45fee1d6143a50543058e6e3ae2b06552581735
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-01-02 20:23:08 +0530

    Time: 1 ms (88.34%) | Memory: 58.6 MB (57.01%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.ts
+++ 724-find-pivot-index/find-pivot-index.ts
@@ -1,14 +1,13 @@
 function pivotIndex(nums: number[]): number {
-    let totalSum = nums.reduce((i,sum)=> sum+=i ,0);
+   let totalSum = nums.reduce((item,sum)=> sum+=item ,0);
+   let leftSum=0;
 
-    let leftSum=0;
 
-    for(let i=0;i<nums.length;i++){
-        let rightSum = totalSum - leftSum - nums[i];
-        if(rightSum === leftSum) return i;
+   for(let i=0;i<nums.length;i++){
+    let rightSum = totalSum - leftSum - nums[i];
+    if(leftSum === rightSum )return i;
 
-        leftSum+=nums[i];
-
-    }
-    return -1;
+    leftSum +=nums[i];
+   }
+   return -1;
 };
