ce73affa85215b4ef014f2e6f670e6c097dc0ea1
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-01-20 23:40:47 +0530

    Time: 2 ms (61.95%) | Memory: 60.3 MB (89.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.ts
+++ 283-move-zeroes/move-zeroes.ts
@@ -3,16 +3,14 @@
  */
 function moveZeroes(nums: number[]): void {
     let j=0;
+
+
     for(let i=0;i<nums.length;i++){
-        if(nums[i]!== 0){
-            nums[j]=nums[i];
-            j++;
-        }
+        if(nums[i]!==0) nums[j++]=nums[i];
     }
 
     while(j<nums.length){
-        nums[j]=0;
-        j++;
+        nums[j++]=0;
     }
     
 };
