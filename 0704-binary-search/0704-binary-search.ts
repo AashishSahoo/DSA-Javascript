116f9542c9b23c7d1923f07dba1adec58d25f728
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-01-24 16:30:38 +0530

    Time: 0 ms (100%), Space: 59.3 MB (6.99%) - LeetHub

--- 0704-binary-search/0704-binary-search.ts
+++ 0704-binary-search/0704-binary-search.ts
@@ -1,20 +1,20 @@
 function search(nums: number[], target: number): number {
     
-    let i=0;
-    let j=nums.length-1;
+let i=0;
+let j=nums.length-1;
 
-    while(i<j){
-        let mid= Math.floor((i+j)/2);
+while(i<=j){
+    let mid =Math.floor((i+j)/2) ;
 
-        if(nums[mid]=== target){
-                        return mid;
-        }
-
-        if(target >nums[mid]){
-            i=mid+1;
-        }else if( target<nums[mid]){
-            j=mid-1;
-        }
+    if(nums[mid]>target){
+        j =mid-1;
+    }else if (nums[mid]<target){
+        i=mid + 1;
+    }else {
+        return mid;
     }
-    return -1;
+
+}
+return -1;
+
 };
