bd7b7b5981ee628d0d19e41e9c33c7fa2feb242e
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-02-01 16:57:44 +0530

    Time: 3 ms (8.16%) | Memory: 54.9 MB (77.33%) - LeetSync

--- 75-sort-colors/sort-colors.ts
+++ 75-sort-colors/sort-colors.ts
@@ -3,12 +3,13 @@
  */
 function sortColors(nums: number[]): void {
     let n= nums.length-1;
-    for(let i=0;i<nums.length;i++){
+    
+    for(let i=0;i<n;i++){
         for(let j=0;j<n-i;j++){
             if(nums[j]>nums[j+1]){
                 let temp=nums[j];
                 nums[j]=nums[j+1];
-                nums[j+1]= temp;
+                nums[j+1]=temp;
             }
         }
     }
