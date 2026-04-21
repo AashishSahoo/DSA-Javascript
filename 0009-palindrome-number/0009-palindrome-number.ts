6730d861b6f6b430638b947838d470789461f3cd
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-04-21 23:53:23 +0530

    Time: 4 ms (92.13%), Space: 64.7 MB (79.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,16 +1,15 @@
 function isPalindrome(x: number): boolean {
-    let s : string = x.toString();
-    let str : number = 0;
-    let end : number = s.length-1; 
- 
-    while(str<end){
-        if(s[str]!==s[end]){
-            return false;
-        }
-        str++;
-        end--;
-        
-    }
-    return true;
+    let arr=x.toString();
 
+    // let arr= x.split("");
+    let i=0;
+    let j=arr.length-1;
+    while(i<j){
+        if(arr[i]!==arr[j]) return false;
+        i++;
+        j--;
+    }
+
+    return true;
+    
 };
