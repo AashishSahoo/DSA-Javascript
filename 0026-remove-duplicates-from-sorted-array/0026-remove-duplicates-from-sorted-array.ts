eac428b02834b283fffa2b7e74329e535074a72c
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-05-03 11:27:07 +0530

    Time: 0 ms (100%), Space: 58.8 MB (46.03%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,13 +1,24 @@
 function removeDuplicates(nums: number[]): number {
+     if (nums.length === 0) {
+        return 0;
+    }
 
-    if(nums.length === 0) return 0;
-    let i=0;
+    let res=1;
 
-    for(let k=1;k<nums.length;k++){
-        if(nums[k]!==nums[i]){
-            i++;
-            nums[i]=nums[k];
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]!==nums[i-1]){
+            nums[res]=nums[i];
+            res++;
         }
     }
-    return i+1;
-}
+    return res;
+
+    // let j = 0; // Pointer for the position of unique elements
+    // for (let i = 1; i < nums.length; i++) {
+    //     if (nums[j] !== nums[i]) {
+    //         j++;
+    //         nums[j] = nums[i]; // Move the unique element to the next position
+    //     }
+    // }
+    // return j + 1;
+};
