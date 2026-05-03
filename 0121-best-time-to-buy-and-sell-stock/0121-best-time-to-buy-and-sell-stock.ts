5245655b02c046e1ab76fe99d0d1b01f5e8f3ea5
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-05-03 11:45:11 +0530

    Time: 2 ms (77.47%), Space: 65.3 MB (90.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,27 +1,20 @@
 function maxProfit(prices: number[]): number {
 
-let min=prices[0];
-let max= prices[0];
-let profit=0;
+ let min = prices[0];
+ let max = prices[0];
+ let profit =0;
 
-
-for(let i=1;i<prices.length;i++){
-    if(min >prices[i]){
-        min = prices[i];
-        max=prices[i];
-        
-    }else{
-        max=prices[i];
+ for(let i=0;i<prices.length;i++){
+    if(prices[i]<min){
+        min= prices[i];
+        max=min;
+    }else if(max<prices[i]){
+        max =prices[i];
     }
-
     let sum = max-min;
-        profit=Math.max(sum,profit);
-        
-}
-
-
-return profit;
-
+    profit=Math.max(profit,sum);
+ }
+ return profit;
 
 
 };
