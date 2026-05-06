fd16d2f0de3556f3ff5469e9be28e51dfae8e552
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-05-06 23:55:54 +0530

    Time: 4 ms (48.47%), Space: 66.1 MB (60.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.ts
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -1,19 +1,17 @@
-/**
- Do not return anything, modify nums in-place instead.
- */
 function rotate(nums: number[], k: number): void {
-  k = k%nums.length-1;
+    
+    k = k%nums.length ;
+    function reverse( start :number , end:number) :void{
+        while(start<end){
+            [nums[start], nums[end]] = [nums[end] ,nums[start]];
+            start++;
+            end--;
+        }
+    }
 
-  function reverse(str,end){
-      while(str<end){
-       let temp = nums[str];
-       nums[str]=nums[end];
-       nums[end]=temp;
-       str++;
-       end--;
-      }
-  }  
-  reverse(0,nums.length-1);
-  reverse(0,k);
-  reverse(k+1,nums.length-1);
+    reverse( 0 , nums.length-1);
+    reverse(0 , k-1);
+    reverse( k,nums.length -1);
+
 };
+
