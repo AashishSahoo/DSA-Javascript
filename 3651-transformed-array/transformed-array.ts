39f368e7665afea84fa7abbbcdb04c740d64dad2
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-06-01 22:44:31 +0530

    Time: 70 ms (7.69%) | Memory: 58.7 MB (26.92%) - LeetSync

--- 3651-transformed-array/transformed-array.ts
+++ 3651-transformed-array/transformed-array.ts
@@ -3,17 +3,8 @@ function constructTransformedArray(nums: number[]): number[] {
     const n = nums.length;
 
     for (let i = 0; i < n; i++) {
-        let newIndex = i + nums[i];
-
-        while (newIndex < 0) {
-            newIndex += n;
-        }
-
-        while (newIndex >= n) {
-            newIndex -= n;
-        }
-
-        result.push(nums[newIndex]);
+        let idx = ((i + nums[i]) % n + n) % n;
+        result[i] = nums[idx];
     }
 
     return result;
