8db15531928d4684a0f410f19ed1f758af3e4879
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-08-20 00:10:35 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (92.64%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,41 +1,12 @@
 function removeDuplicates(nums: number[]): number {
+    let k = 1;
 
-
-
- if(nums.length === 0) return 0;
-
-
- let res=1;
- for(let i=1;i<nums.length;i++){
-    if(nums[i]!==nums[i-1]){
-        nums[res]=nums[i];
-        res++;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
     }
- }
 
- return res;
-
-//      if (nums.length === 0) {
-//         return 0;
-//     }
-
-
-//    let res=1;
-
-//    for(let i=1;i<nums.length;i++){
-//     if(nums[i]!==nums[i-1]){
-//         nums[res]=nums[i];
-//         res++;
-//     }
-//    }
-//    return res;
-
-    // let j = 0; // Pointer for the position of unique elements
-    // for (let i = 1; i < nums.length; i++) {
-    //     if (nums[j] !== nums[i]) {
-    //         j++;
-    //         nums[j] = nums[i]; // Move the unique element to the next position
-    //     }
-    // }
-    // return j + 1;
-};
+    return k;
+}
