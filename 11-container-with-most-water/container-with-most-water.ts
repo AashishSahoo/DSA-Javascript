4fd4e3547da6ad6e3d47a6dd02a36868f3e01de0
Ashish Sahoo <103375094+AashishSahoo@users.noreply.github.com>
2026-08-23 23:21:16 +0530

    Time: 3 ms (38.28%) | Memory: 63.2 MB (80.98%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -1,27 +1,24 @@
 function maxArea(height: number[]): number {
-    
+  let left =0;
+  let right = height.length-1;
 
-    let n= height.length -1;
+  let res=0;
 
+  while(left<right){
 
-    let left =0;
-    let right = n;
-    let result = 0;
+    let width = right-left;
+    let ht = Math.min(height[left],height[right]);
+    let area = ht*width;
 
-    while(left <right ){
-        let ht = Math.min( height[left] , height[right]);
-        let width = right -left ;
-        let area = ht*width;
-        result = Math.max(result ,area);
+    res= Math.max(area , res);
 
-        if( height[left] < height[right]){
-            left++;
-        }else{
-            right --;
-        }
-
-
+    if(height[left]<height[right]){
+        left++;
+    }else {
+        right--;
     }
 
-    return result;
+
+  }
+  return res;
 };
